4baec0d9e62488110eb00479cd9b774c0df95903
sastra12 <anonymousonly594@gmail.com>
2024-11-20 11:11:46 +0700

    delete admin lte

--- resources/views/auth/auth.blade.php
+++ resources/views/auth/auth.blade.php
@@ -7,69 +7,55 @@
     <title>Log in</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE-3/plugins/fontawesome-free/css/all.min.css') }}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE-3/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE-3/dist/css/adminlte.min.css') }}">
-    <!-- Google Font: Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous" />
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}" />
 </head>
 
-<body class="hold-transition login-page images-bg">
-    <div class="background-image-login"></div>
-    <div class="login-box">
-        <div class="card">
-            <div class="card-body login-card-body">
-                @if (session('status'))
-                    <div class="alert alert-danger">
-                        {{ session('status') }}
-                    </div>
-                @endif
-                <form action="{{ route('authenticate') }}" method="post">
-                    @csrf
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" autocomplete="off" value="{{ old('username') }}"
-                            class="form-control @error('username') is-invalid @enderror" id="username" name="username">
-                        @error('username')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control @error('password') is-invalid @enderror"
-                            id="exampleInputPassword1" name="password">
-                        @error('password')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
+<body>
+    <div class="container h-100">
+        <div class="row justify-content-center align-items-center h-100">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body login-card-body">
+                        @if (session('status'))
+                            <div class="alert alert-danger">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <form action="{{ route('authenticate') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" autocomplete="off" value="{{ old('username') }}"
+                                    class="form-control @error('username') is-invalid @enderror" id="username"
+                                    name="username">
+                                @error('username')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                    id="exampleInputPassword1" name="password">
+                                @error('password')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
 
-                    <div class="row">
-                        <!-- /.col -->
-                        <div class="col-md">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                        </div>
-                        <!-- /.col -->
+                            <div class="row">
+                                <!-- /.col -->
+                                <div class="col-md">
+                                    <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                                </div>
+                                <!-- /.col -->
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
-        <!-- /.login-box -->
-
-        <!-- jQuery -->
-        <script src="{{ asset('AdminLTE-3/plugins/jquery/jquery.min.js') }}"></script>
-        <!-- Bootstrap 4 -->
-        <script src="{{ asset('AdminLTE-3/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <!-- AdminLTE App -->
-        <script src="{{ asset('AdminLTE-3/dist/js/adminlte.min.js') }}"></script>
-
+    </div>
 </body>
 
 </html>
